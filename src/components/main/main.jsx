5643d3ac82c277229242c424ae32fb7b99ead313
ilkin1Zahidov <ilkinzahidov01@gmail.com>
2023-02-15 00:40:40 +0400

    fixed menu

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,65 +1,57 @@
-import React from 'react';
+import React from "react";
 import DataJson from "../../data/data";
 
 const main = ({ image }) => {
-    return (
-        <>
-            <section id='main'>
-                <div className="container">
-                    <div className="card--all">
-                        {
-                            DataJson.map((data) => {
-                                return (
-                                    <div className="card">
-                                     
-                                            <div 
-                                            
-                                            key={data.id}
-                                            style={{
-                                                backgroundImage: `url(${data.picture})`,
-                                                height: data.height,
-                                                width: data.width,
-                                                backgroundRepeat:data.backgroundRepeat
-                                            }}
-                                            />
-                                  
-                                        <div className="card-body">
-                                            <h1 className='card_name'>Apple Watch</h1>
-                                            <p className='card_series'>Series 5 SE</p>
-                                            <div className="card_money">
-                                                <span className='money'>$ 529.99</span>
-                                                <form id='checkout'>
-                                                    <button className='card_button'><img src={image.Basket} alt="" /></button>
-                                                </form>
-                                            </div>
-                                        </div>
-                                    </div>
-                                )
-                            })
-                        }
+  return (
+    <>
+      <section id="main">
+        <div className="container">
+          <div className="card--all">
+            {DataJson.map((data) => {
+              return (
+                <div className="card">
+                  <div
+                    key={data.id}
+                    style={{
+                      width: data.size,
+                      height: data.sizeHeight,
+                      background: data.background,
+                      borderRadius: data.borderRadius,
+                      padding: data.padding,
+                    }}
+                  >
+                    <div
+                      key={data.id}
+                      style={{
+                        backgroundImage: `url(${data.picture})`,
+                        height: data.height,
+                        width: data.width,
+                        backgroundRepeat: data.backgroundRepeat,
+                        borderRadius: data.borderRadius,
+                      }}
+                    />
+                  </div>
 
-                        {/* <div style={{ width: '460px', height: "360px" }} className="card">
-                            <div className="card_img">
-                                <img src={image.Card_five} alt="card_one" />
-                            </div>
-                            <div className="card-body">
-                                <h1 className='card_name'>Apple Watch</h1>
-                                <p className='card_series'>Series 5 SE</p>
-                                <div className="card_money">
-                                    <span className='money'>$ 529.99</span>
-                                    <form id='checkout'>
-                                        <button className='card_button'><img src={image.Basket} alt="" /></button>
-                                    </form>
-                                </div>
-                            </div>
-                        </div> */}
-
-
+                  <div className="card-body">
+                    <h1 className="card_name">Apple Watch</h1>
+                    <p className="card_series">Series 5 SE</p>
+                    <div className="card_money">
+                      <span className="money">$ 529.99</span>
+                      <form id="checkout">
+                        <button className="card_button">
+                          <img src={data.basket} alt="basket" />
+                        </button>
+                      </form>
                     </div>
+                  </div>
                 </div>
-            </section>
-        </>
-    )
-}
+              );
+            })}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default main
+export default main;
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -2,16 +2,13 @@ import React from 'react';
 import Input from '../components/input/input';
 import Main from '../components/main/main';
 
-import Basket from '../assets/img/basket.svg';
 
 
 const Index = () => {
   return (
     <>
         <Input/>
-        <Main
-          image = {Basket}
-        />
+        <Main/>
     </>
   )
 }
